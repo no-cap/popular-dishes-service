d13fb66c041dc096343165bc330783709c7bfb26
rory-roccio <rory.roccio@gmail.com>
2020-03-31 12:33:48 -0700

    Format schema and remove comments

--- database/schema.sql
+++ database/schema.sql
@@ -3,12 +3,10 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';
 DROP DATABASE IF EXISTS PopularDishesList;
 
 CREATE DATABASE PopularDishesList;
--- SELECT host, user
--- -- FROM mysql.user;
+
 USE PopularDishesList;
 
-CREATE TABLE Restaurants
-(
+CREATE TABLE Restaurants (
     restaurant_id int NOT NULL
     AUTO_INCREMENT,
     restaurant_name varchar
@@ -17,80 +15,48 @@ CREATE TABLE Restaurants
     (restaurant_id)
 );
 
--- INSERT INTO Restaurants (restaurant_name) values ('john');
-
-    CREATE TABLE PopularDishes
-    (
-        dish_id int NOT NULL
-        AUTO_INCREMENT,
-    dish_name varchar
-        (50) NOT NULL,
-    price decimal
-        (10, 2) NOT NULL,
-    description varchar
-        (1000) NOT NULL,
+CREATE TABLE PopularDishes (
+    dish_id int NOT NULL
+    AUTO_INCREMENT,
+    dish_name varchar (50) NOT NULL,
+    price decimal (10, 2) NOT NULL,
+    description varchar (1000) NOT NULL,
     review_count INT NOT NULL,
     restaurant int,
-    PRIMARY KEY
-        (dish_id),
-    FOREIGN KEY
-        (restaurant) REFERENCES Restaurants
-        (restaurant_id)
+    PRIMARY KEY (dish_id),
+    FOREIGN KEY (restaurant) REFERENCES Restaurants (restaurant_id)
 );
 
-
-        CREATE TABLE photos
-        (
-            photo_id int NOT NULL
-            AUTO_INCREMENT,
-    url varchar
-            (200) NOT NULL,
-    caption varchar
-            (1000) NOT NULL,
+CREATE TABLE photos (
+    photo_id int NOT NULL AUTO_INCREMENT,
+    url varchar (200) NOT NULL,
+    caption varchar (1000) NOT NULL,
     popular_dish int,
-    PRIMARY KEY
-            (photo_id),
-    FOREIGN KEY
-            (popular_dish) REFERENCES PopularDishes
-            (dish_id)
+    PRIMARY KEY (photo_id),
+    FOREIGN KEY (popular_dish) REFERENCES PopularDishes (dish_id)
 );
 
 
-            CREATE TABLE users
-            (
-                userid int NOT NULL
-                AUTO_INCREMENT,
-    userphoto VARCHAR
-                (200),
-    username VARCHAR
-                (70) NOT NULL,
+CREATE TABLE users (
+    userid int NOT NULL AUTO_INCREMENT,
+    userphoto VARCHAR (200),
+    username VARCHAR (70) NOT NULL,
     friends int, 
     reviews int,
-    PRIMARY KEY
-                (userid),
-    UNIQUE
-                (username)
+    PRIMARY KEY (userid),
+    UNIQUE (username)
 );
 
-                CREATE TABLE reviews
-                (
-                    reviewid int NOT NULL
-                    AUTO_INCREMENT,
+CREATE TABLE reviews (
+    reviewid int NOT NULL AUTO_INCREMENT,
     userid int NOT NULL,
-    date VARCHAR
-                    (100) NOT NULL,
+    date VARCHAR (100) NOT NULL,
     rating int NOT NULL,
-    text VARCHAR
-                    (1000) NOT NULL,
+    text VARCHAR (1000) NOT NULL,
     dish_id int NOT NULL,
-    PRIMARY KEY
-                    (reviewid),
-    FOREIGN KEY
-                    (userid) REFERENCES users
-                    (userid),
-    FOREIGN KEY
-                    (dish_id) REFERENCES PopularDishes
-                    (dish_id)  
+    PRIMARY KEY (reviewid),
+    FOREIGN KEY (userid) REFERENCES users (userid),
+    FOREIGN KEY (dish_id) REFERENCES PopularDishes (dish_id)  
 );
 
 
