48fd63ef4b4b6b87e93ef6ccd3f94cf99e642767
rory-roccio <rory.roccio@gmail.com>
2020-03-31 15:31:05 -0700

    Add .env configuration and server start error handling

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const companyNumber = 10; // this is how many companies there are;
 const controller = require('./controller.js');
-const port = 3000;
 
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(process.env.SERVER_PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server is running on', + process.env.SERVER_PORT)
+  }
+});
+
 app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, '../client/dist/')))
@@ -16,7 +22,7 @@ app.use('/', express.static(path.join(__dirname, '../client/dist/')))
 app.get('/popularDishes/getCompany', (req, res) => {
   // var companyId = Math.floor(Math.random() * companyNumber) + 1;
   controller.getCompany(companyNumber, res);
-})
+});
 
 app.get('/popularDishes/getItems', (req, res) => {
   controller.getDishes(req, res);
@@ -25,7 +31,7 @@ app.get('/popularDishes/getItems', (req, res) => {
 app.get('/popularDishes/getPhotos', (req, res) => {
   console.log(req.query);
   controller.getPhotos(req, res);
-})
+});
 
 app.get('/popularDishes/arrow.png', (req, res) => {
   res.sendFile();
@@ -37,8 +43,4 @@ app.get('/popularDishes/leftarrow.png', (req, res) => {
 
 app.get('/popularDishes/getReviews', (req, res) => {
   controller.getReviews(req, res);
-})
-
-app.listen(port, () => {
-  console.log('server is running on', + port)
-})
+});
