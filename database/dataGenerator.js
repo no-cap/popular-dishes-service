6a7731e106466860b6c86db8d8ce401d9758c9e6
rory-roccio <rory.roccio@gmail.com>
2020-03-31 23:16:54 -0700

    Lint with AirBnB style guide

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
+const faker = require('faker');
 const config = require('../config.json');
 const db = require('./index.js');
-const faker = require('faker');
 
 // how many companies do we want to be made
 const numberOfCompanies = 10;
@@ -10,88 +10,76 @@ const numberOfCompanies = 10;
 const maxDishes = 9;
 
 // Max number of reviews per dish
-const maxReviews = 100;
+const maxReviews = 20;
 
 // max number of photos per dish
-const maxPhotos = 20;
+const maxPhotos = 8;
 
 // region and bucket for aws
-const photoBucket = 'photosthree';
-const westRegion = 'us-west-1';
+const bucket = 'sdc-food';
+const region = 'us-west-1';
 
 // function for making one company
-const makeCompany = () => {
-  return new Promise((resolve, reject) => {
-    var params = [faker.company.companyName()]
-    var query = 'INSERT INTO restaurants (restaurant_name) values(?)'
-    db.query(query, params, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
+const makeCompany = () => new Promise((resolve, reject) => {
+  const params = [faker.company.companyName()];
+  const query = 'INSERT INTO restaurants (restaurant_name) values(?)';
+  db.query(query, params, (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data);
+    }
   });
-};
+});
 
 // function for making one dish
-const makeDish = (addDish, params) => {
-  return new Promise((res, rej) => {
-    db.query(addDish, params, (err, data) => {
-      if (err) {
-        rej(err)
-      } else {
-        res(data)
-      }
-    });
+const makeDish = (addDish, params) => new Promise((res, rej) => {
+  db.query(addDish, params, (err, data) => {
+    if (err) {
+      rej(err);
+    } else {
+      res(data);
+    }
   });
-};
+});
 
 // function for making one photo
-const makePhoto = (addPhoto, photoParams) => {
-  return new Promise((res, rej) => {
-    db.query(addPhoto, photoParams, (err) => {
-      if (err) {
-        rej(err)
-      } else {
-        res();
-      }
-    });
+const makePhoto = (addPhoto, photoParams) => new Promise((res, rej) => {
+  db.query(addPhoto, photoParams, (err) => {
+    if (err) {
+      rej(err);
+    } else {
+      res();
+    }
   });
-};
+});
 
 // function for making one review;
-const makeReview = (reviewParams) => {
-  return new Promise((res, rej) => {
-    const query = 'INSERT INTO reviews (userid, date, rating, text, dish_id) values (?,?,?,?,?)';
-    db.query(query, reviewParams, (err) => {
-      if (err) {
-        rej(err)
-      } else {
-        res();
-      }
-    });
+const makeReview = (reviewParams) => new Promise((res, rej) => {
+  const query = 'INSERT INTO reviews (userid, date, rating, text, dish_id) values (?,?,?,?,?)';
+  db.query(query, reviewParams, (err) => {
+    if (err) {
+      rej(err);
+    } else {
+      res();
+    }
   });
-};
+});
 
-//make users
-const makeUser = (imageUrl) => {
-  return new Promise((res, rej) => {
-    const userParams = [faker.name.findName(), imageUrl, Math.floor(Math.random() * 100), Math.floor(Math.random() * 20)];
-    const query = 'INSERT INTO users (username, userphoto, reviews, friends) values (?, ?, ?, ?)';
-    db.query(query, userParams, (err, data) => {
-      if (err) {
-        rej(err)
-      } else {
-        res(data);
-      }
-    });
+// make users
+const makeUser = (imageUrl) => new Promise((res, rej) => {
+  const userParams = [faker.name.findName(), imageUrl, Math.floor(Math.random() * 100), Math.floor(Math.random() * 20)];
+  const query = 'INSERT INTO users (username, userphoto, reviews, friends) values (?, ?, ?, ?)';
+  db.query(query, userParams, (err, data) => {
+    if (err) {
+      rej(err);
+    } else {
+      res(data);
+    }
   });
-};
+});
 
-const formatUrlWithKey = (object, region) => {
-  return 'https://photosthree.s3-' + region + '.amazonaws.com/' + object.Key;
-};
+const formatUrlWithKey = (object) => `https://${bucket}.s3-${region}.amazonaws.com/${object.key}`;
 
 (async function () {
   try {
@@ -99,72 +87,59 @@ const formatUrlWithKey = (object, region) => {
     aws.config.update({
       accessKeyId: config.aws.accessKey,
       secretAccessKey: config.aws.secretKey,
-      region: westRegion
+      region,
     });
 
     const s3 = new aws.S3();
 
-    const response = await s3.listObjectsV2({
-      Bucket: photoBucket,
-      Prefix: 'Random Foods' // folder names can be changed here
+    const response1 = await s3.listObjectsV2({
+      Bucket: bucket,
     }).promise();
-  
+
     const response2 = await s3.listObjectsV2({
-      Bucket: photoBucket,
-      Prefix: 'Random Foods 2' //folder names can be changed here
+      Bucket: bucket,
     }).promise();
 
-    const response3 = await s3.listObjectsV2({
-      Bucket: photoBucket,
-      Prefix: 'main-sprites' // had three folders, but you should ideally have two folders
-    }).promise();
-  
-    var arrayOfObjects = response.Contents.concat(response2.Contents)
-    var arrayOfProfiles = response3.Contents;
+    const imagesArray = response1.Contents;
+    const avatarsArray = response2.Contents;
 
-    for (var i = 0; i < numberOfCompanies; i++) {
+    for (let i = 0; i < numberOfCompanies; i += 1) {
       makeCompany()
-        .then(data => {
+        .then((data) => {
           // after making one company we get it's restaurantid
-          var restaurantId = data.insertId;
-          console.log('made restaurant with restaurant_id', restaurantId)
-          for (var i = 0; i < maxDishes; i++) {
-            var reviewCount = Math.round(Math.random() * maxReviews);
-            console.log('----------------------------------------', reviewCount);
-            var params = [faker.lorem.word(), faker.random.number(), faker.lorem.words(), reviewCount, restaurantId]
-            var addDish = 'INSERT INTO popular_dishes (dish_name, price, description, review_count, restaurant) values (?,?,?,?,?)';
+          const restaurantId = data.insertId;
+          for (let i = 0; i < maxDishes; i += 1) {
+            const reviewCount = Math.round(Math.random() * maxReviews);
+            const params = [faker.lorem.word(), faker.random.number(), faker.lorem.words(), reviewCount, restaurantId];
+            const addDish = 'INSERT INTO popular_dishes (dish_name, price, description, review_count, restaurant) values (?,?,?,?,?)';
             // then we make multiple dishes with the restaurantid as it's foreign key
             makeDish(addDish, params)
-              .then(data => {
-                var dish_id = data.insertId;
-                for (var o = 0; o < Math.floor(Math.random() * maxPhotos) + 1; o++) {
-                  var addPhoto = 'INSERT INTO photos (url, caption, popular_dish) values (?,?,?)';
-                  var randomObject = arrayOfObjects[Math.floor(Math.random() * arrayOfObjects.length)];
-                  var photoUrl = formatUrlWithKey(randomObject, westRegion);
-                  var photoParams = [photoUrl, faker.lorem.words(), dish_id];
-                  makePhoto(addPhoto, photoParams).catch(e => console.log('from the make photo',e))
+              .then((data) => {
+                const dishId = data.insertId;
+                for (let o = 0; o < Math.floor(Math.random() * maxPhotos) + 1; o += 1) {
+                  const addPhoto = 'INSERT INTO photos (url, caption, popular_dish) values (?,?,?)';
+                  const randomObject = imagesArray[Math.floor(Math.random() * imagesArray.length)];
+                  const photoUrl = formatUrlWithKey(randomObject);
+                  const photoParams = [photoUrl, faker.lorem.words(), dishId];
+                  makePhoto(addPhoto, photoParams).catch(() => console.log('Failed to add photo...'));
                 }
 
-                for (var k = 0; k < params[3]; k++) {
-                  var imageUrl = formatUrlWithKey(arrayOfProfiles[Math.floor(Math.random() * arrayOfObjects.length)], westRegion);
+                for (let k = 0; k < params[3]; k += 1) {
+                  const imageUrl = formatUrlWithKey(avatarsArray[Math.floor(Math.random() * imagesArray.length)]);
                   makeUser(imageUrl)
-                    .then(response => {
-                      var userid = response.insertId;
-                      var reviewParams = [userid, faker.date.past(1), Math.ceil(Math.random() * 5), faker.lorem.sentences(), dish_id];
+                    .then((response) => {
+                      const userid = response.insertId;
+                      const reviewParams = [userid, faker.date.past(1), Math.ceil(Math.random() * 5), faker.lorem.sentences(), dishId];
                       makeReview(reviewParams)
                         .then(() => {
-                          console.log('reviews have been made');
-                        }).catch(e => console.log('from the make review', e))
-                    }).catch(e => console.log('from the make user', e))
+                        }).catch(() => console.log('Failed to add review'));
+                    }).catch(() => console.log('Failed to add user'));
                 }
-              }).catch(e => console.log('from the make dish', e));
+              }).catch(() => console.log('Failed to add dish'));
           }
-        }).catch(e => console.log('from the make company',e))
+        }).catch(() => console.log('Failed to add restaurant'));
     }
+  } catch (e) {
+    console.log('Error: ', e);
   }
-  
-  catch (e) {
-    console.log('our error from outside', e);
-  }
-
-})();
+}());
