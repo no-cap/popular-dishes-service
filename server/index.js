4aae750abbe27d9667e369c2d2da22e515d1284d
rory-roccio <rory.roccio@gmail.com>
2020-03-31 23:17:53 -0700

    Lint and remove console errors

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const companyNumber = 10; // this is how many companies there are;
 const controller = require('./controller.js');
+
+const app = express();
+const companyNumber = 10; // this is how many companies there are;
 
 app.listen(process.env.SERVER_PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('Server is running on', + process.env.SERVER_PORT)
+    console.log(`Server is running on ${process.env.SERVER_PORT}`);
   }
 });
 
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../client/dist/')))
+app.use('/', express.static(path.join(__dirname, '../client/dist/')));
 
 
 app.get('/popularDishes/getCompany', (req, res) => {
-  // var companyId = Math.floor(Math.random() * companyNumber) + 1;
   controller.getCompany(companyNumber, res);
 });
 
@@ -29,8 +29,11 @@ app.get('/popularDishes/getItems', (req, res) => {
 });
 
 app.get('/popularDishes/getPhotos', (req, res) => {
-  console.log(req.query);
   controller.getPhotos(req, res);
+});
+
+app.get('/popularDishes/getReviews', (req, res) => {
+  controller.getReviews(req, res);
 });
 
 app.get('/popularDishes/arrow.png', (req, res) => {
@@ -38,9 +41,5 @@ app.get('/popularDishes/arrow.png', (req, res) => {
 });
 
 app.get('/popularDishes/leftarrow.png', (req, res) => {
-  res.sendFile('https://photosthree.s3-us-west-1.amazonaws.com/leftarrow.png');
-});
-
-app.get('/popularDishes/getReviews', (req, res) => {
-  controller.getReviews(req, res);
+  res.sendFile('/leftarrow.png');
 });
