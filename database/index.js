68e7d43e527d9e9c56f1de44a40bd8b68d495868
rory-roccio <rory.roccio@gmail.com>
2020-03-31 23:17:06 -0700

    Require dotenv

--- database/index.js
+++ database/index.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
 });
 
 connection.connect((err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`Connected to ${process.env.MYSQL_DB}`)
+    console.log(`Connected to ${process.env.MYSQL_DB}`);
   }
 });
 
