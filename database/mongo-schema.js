804f92badd576552ca20d2663f31c884ab99b873
rory-roccio <rory.roccio@gmail.com>
2020-04-01 20:40:24 -0700

    Set general json schema format for mongoDB

---
+++ database/mongo-schema.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+db.createCollection("restaurants", {
+
+});
+
+/*
+{
+  "restaurant_id": "<varchar>",
+  "restaurant_name": "<str>",
+  "location" : <geoson>,
+  "popularDishes": [
+    {
+      "dish_id": "<varchar>",
+      "dish_name": "<str>",
+      "price": "<num>",
+      "description": "<text>",
+      "reviews": [
+        {
+          "review_id": "<varchar>",
+          "date_time": "<date>",
+          "rating": "<int>",
+          "review_text": "<text>",
+          "user": {
+            "user_id": "<varchar>",
+            "username": "<varchar>",
+            "userphoto": "<varchar>"
+          },
+          "photo": {
+            "photo_id": "<varchar>",
+            "url": "<varchar>",
+            "caption": "<varchar>"
+          }
+        }
+      ]
+    }
+  ]
+}
+*/
