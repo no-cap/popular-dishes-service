5e715ea8d8c60d1414a8cc0babbfbe220a63fcaa
rory-roccio <rory.roccio@gmail.com>
2020-04-06 11:36:27 -0700

    Add command line argument processing

--- database/dbseeding/generateUsers.js
+++ database/dbseeding/generateUsers.js
@@ -9,9 +9,9 @@ const AllUsers = []; // need 1 per review (40-80M)
 /* USERS */
 // user profile images > https://sdc-users.s3-us-west-1.amazonaws.com/1.png
 const createUsers = () => {
-  for (let i = 0; i < 17; i += 1) {
+  for (let i = 0; i < process.argv[2]; i += 1) {
     const set = [];
-    for (let ii = 0; ii < 200000; ii += 1) {
+    for (let ii = 0; ii < process.argv[3]; ii += 1) {
       set.push({
         username: faker.name.firstName(),
         userPhoto: `https://sdc-users.s3-us-west-1.amazonaws.com/${randomNum(0, 71)}.png`,
@@ -22,4 +22,4 @@ const createUsers = () => {
 };
 
 createUsers();
-writeAllToJSON(AllUsers, 'users');
+writeAllToJSON(AllUsers, 'users', process.argv[4]);
