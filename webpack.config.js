0b00c23f0fbf41b437d60cac64bf28f516b18c15
rory-roccio <rory.roccio@gmail.com>
2020-04-07 12:28:01 -0700

    Add file extension resolving

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const path = require('path');
+
 module.exports = {
-    entry: './client/src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './client/dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /.jsx$|.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }
-            }
-        ]
-    }
-}
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx$|.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css'],
+  },
+};
