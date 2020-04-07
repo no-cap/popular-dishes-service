98710400d7fa33a26508f4332a149eb510035067
rory-roccio <rory.roccio@gmail.com>
2020-04-06 18:22:03 -0700

    Add outline for all CRUD API routes

--- server/controllers/mongo.controller.js
+++ server/controllers/mongo.controller.js
@@ -5,6 +5,7 @@ const Model = require('../models/mongo.model.js');
   * POST ROUTES
 */
 
+// /api/restaurants/:restaurantID/dishes
 module.exports.postDish = (req, res) => {
   const newDish = req.body;
   Model.postDish(newDish, (err, result) => {
@@ -17,9 +18,98 @@ module.exports.postDish = (req, res) => {
   });
 };
 
+// /api/restaurants/:restaurantID/dishes/:dishID/reviews
 module.exports.postReview = (req, res) => {
   const newReview = req.body;
-  Model.postDish(newReview, (err, result) => {
+  Model.postReview(newReview, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
+
+/*
+  * GET ROUTES
+*/
+
+// /api/restaurants/:restaurantID/dishes
+module.exports.getDishes = (req, res) => {
+  Model.getDishes((err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(results);
+    }
+  });
+};
+
+// /api/restaurants/:restaurantID/nearby
+module.exports.getNearby = (req, res) => {
+  Model.getNearby((err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(results);
+    }
+  });
+};
+
+/*
+  * PUT ROUTES
+*/
+
+// /api/restaurants/:restaurantID/dishes/:dishID
+module.exports.putDish = (req, res) => {
+  const updatedDish = req.body;
+  Model.putDish(updatedDish, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
+
+// /api/restaurants/:restaurantID/dishes/:dishID/reviews/:reviewID
+module.exports.putReview = (req, res) => {
+  const updatedReview = req.body;
+  Model.putReview(updatedReview, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
+
+/*
+  * DELETE ROUTES
+*/
+
+// /api/restaurants/:restaurantID/dishes/:dishID
+module.exports.deleteDish = (req, res) => {
+  const deletedDish = req.body;
+  Model.deleteDish(deletedDish, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
+
+// /api/restaurants/:restaurantID/dishes/:dishID/reviews/:reviewID
+module.exports.deleteReview = (req, res) => {
+  const deletedReview = req.body;
+  Model.deleteReview(deletedReview, (err, result) => {
     if (err) {
       console.log(err);
       res.status(400).send();
