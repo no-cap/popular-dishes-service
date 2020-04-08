9a19bdc708977a4fa779cef1ae92772e0f843071
rory-roccio <rory.roccio@gmail.com>
2020-04-07 20:08:08 -0700

    Add prop destructuring

--- client/src/components/MenuItem/MenuItem.jsx
+++ client/src/components/MenuItem/MenuItem.jsx
@@ -5,25 +5,29 @@ import Camera from '../Camera/Camera';
 import { StarLocation, Item, SquarePhoto, PhotoDescription, Name, Price, TextDescription, Reviews, CameraFormat, StarFormat, Photos } from '../FullMenu/FullMenuStyles';
 
 
-const MenuItem = ({ data }) => (
-  <Item>
-    <SquarePhoto>
-      <img src={(data.photos.length > 0) ? data.photos[0].url : 'https://s3-media0.fl.yelpcdn.com/assets/2/www/img/dca54f97fb84/default_avatars/menu_medium_square.png'} />
-    </SquarePhoto>
-    <PhotoDescription>
-      <Name>{data.item.dish_name}</Name>
-      <TextDescription>{data.item.description}</TextDescription>
-      <StarLocation>
-        <StarFormat>
-          <Star />
-        </StarFormat>
-      </StarLocation>
-      <Reviews>{(data.item.review_count > 1) ? `${data.item.review_count} reviews` : `${data.item.review_count} review`}</Reviews>
-      {(data.photos.length) ? <CameraFormat><Camera /></CameraFormat> : null}
-      <Photos>{(data.photos.length > 1) ? `${data.photos.length} photos` : (data.photos.length === 0) ? null : `${data.photos.length} photo`}</Photos>
-    </PhotoDescription>
-    <Price>{`${data.item.price}`}</Price>
-  </Item>
-);
+const MenuItem = ({ dish }) => {
+  const { reviews } = dish;
+  const photos = reviews.map((review) => review.photoUrl);
+  return (
+    <Item>
+      <SquarePhoto>
+        <img src={(photos.length > 0) ? photos[0] : 'https://s3-media0.fl.yelpcdn.com/assets/2/www/img/dca54f97fb84/default_avatars/menu_medium_square.png'} />
+      </SquarePhoto>
+      <PhotoDescription>
+        <Name>{dish.dishName}</Name>
+        <TextDescription>{dish.description}</TextDescription>
+        <StarLocation>
+          <StarFormat>
+            <Star />
+          </StarFormat>
+        </StarLocation>
+        <Reviews>{`${reviews.length} review${reviews.length > 1 ? 's' : ''}`}</Reviews>
+        {(photos.length) ? <CameraFormat><Camera /></CameraFormat> : null}
+        <Photos>{`${photos.length} photo${photos.length > 1 ? 's' : ''}`}</Photos>
+      </PhotoDescription>
+      <Price>{`${dish.price}`}</Price>
+    </Item>
+  );
+};
 
 export default MenuItem;
