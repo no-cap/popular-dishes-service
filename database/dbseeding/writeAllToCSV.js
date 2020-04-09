b85ed2fdcb4516912942e0f05ff05b002beb5eea
rory-roccio <rory.roccio@gmail.com>
2020-04-08 19:03:25 -0700

    Edit script to write one large file rather than multiple files, remove trailing commas

--- database/dbseeding/writeAllToCSV.js
+++ database/dbseeding/writeAllToCSV.js
@@ -1,91 +1,48 @@
+/* eslint-disable no-nested-ternary */
 const fs = require('fs');
+const cliProgress = require('cli-progress');
+const { restaurant, dish, review, user } = require('./genDishesCSV');
 
-const testData_restaurant1 = [
-  '1245243',
-  'Charleys',
-];
+const model = process.argv[2] === 'restaurants' ? restaurant
+  : process.argv[2] === 'dishes' ? dish
+    : process.argv[2] === 'reviews' ? review
+      : process.argv[2] === 'users' ? user
+        : restaurant;
 
-const testData_restaurant2 = [
-  '327531',
-  'Macs',
-];
+const numberOfRecords = parseInt(process.argv[3], 10);
+const isContinuation = process.argv[5] || false;
 
-const testData_dishes1 = [
-  '67456483',
-  'Super Manly but non-gendered Salad',
-  10.99,
-  'Nothing not-manly about this one... No sirree',
-  '1245243',
-];
-
-const testData_dishes2 = [
-  '3223621',
-  'Impossible Burger',
-  12.99,
-  'A super delicious burger to relive you of hunger',
-  '327531',
-];
-
-const restaurantHeaders = ['id', 'restaurant_name'];
-const dishesHeaders = ['id', 'dish_name', 'price', 'description', 'restaurant_id'];
-
-const testData_restaurants = {
-  headers: restaurantHeaders,
-  data: [[testData_restaurant1, testData_restaurant2],
-    [testData_restaurant1, testData_restaurant2]],
-};
-const testData_dishes = {
-  headers: dishesHeaders,
-  data: [[testData_dishes1, testData_dishes2], [testData_dishes1, testData_dishes2]],
-};
-const allData = [testData_restaurants, testData_dishes];
+// create new progress bar
+const ProgressBar = new cliProgress.SingleBar({
+  format: `|| {bar} || {percentage}% || {value}/{total} Records || ${model.table}`,
+  stopOnComplete: true,
+}, cliProgress.Presets.shades_grey);
+ProgressBar.start(numberOfRecords, 0);
 
 // Function that returns a write stream to a file called prefixCOUNT.csv
-const openStream = (prefix) => (
-  fs.createWriteStream(`../${prefix}.csv`, {
+const filePath = `../${model.table}.csv`;
+const openStream = (filePath) => (
+  fs.createWriteStream(filePath, {
     flags: 'a',
   })
 );
 
-// This creates an async forEach method which we will use to write records to a json file
-async function asyncForEach(array, callback) {
-  for (let i = 0; i < array.length; i += 1) {
-    // eslint-disable-next-line no-await-in-loop
-    await callback(array[i], i, array);
-  }
-}
-
-/*
-  * This fuction writes an array records to a csv file,
-*/
-const writeData = (stream, array) => {
-  const writeAsync = async (inputStream) => {
-    await asyncForEach(array, async (record) => {
-      const stringified = JSON.stringify(record);
-      const trimmedRow = stringified.slice(1, stringified.length - 1);
-      await inputStream.write(`${trimmedRow},\n`);
-    });
-  };
-  writeAsync(stream);
+// This function takes an array and turns it into a string without brackets
+const trimArray = (array) => {
+  const stringified = JSON.stringify(array);
+  return stringified.slice(1, stringified.length - 1);
 };
 
-/*
-  * This fuction takes an array of arrays of records and calls
-  * writeData function on each, writing each set to a csv file
-*/
-const writeAllData = (dataObj, prefix) => {
-  const stringified = JSON.stringify(dataObj.headers);
-  const trimmedHeaders = stringified.slice(1, stringified.length - 1);
-  const writer = openStream(prefix);
-  writer.write(`${trimmedHeaders},\n`, () => {
-    const writeArrayOfSetsAsync = async () => {
-      await asyncForEach(dataObj.data, async (set) => {
-        writeData(writer, set);
+const writeRecords = (model) => {
+  const writer = fs.createWriteStream(filePath, { flags: 'a' });
+  writer.write(`${!isContinuation ? `${trimArray(model.headers)}\n` : ''}`, () => {
+    const writer2 = fs.createWriteStream(filePath, { flags: 'a' });
+    for (let i = 0; i < numberOfRecords; i += 1) {
+      writer2.write(`${trimArray(model())}${i === model.headers.length - 1 ? ',' : ''}\n`, () => {
+        ProgressBar.increment();
       });
-    };
-    writeArrayOfSetsAsync(writer);
+    }
   });
 };
-// writeAllData(testData_restaurants, 'restaurants');
-// writeAllData(testData_dishes, 'dishes');
-module.exports.writeAllToCSV = writeAllData;
+
+writeRecords(model);
