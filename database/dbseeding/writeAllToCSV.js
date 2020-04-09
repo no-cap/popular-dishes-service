db3e2df7537967f5b7606072bf0899d3d6ace195
rory-roccio <rory.roccio@gmail.com>
2020-04-08 22:21:15 -0700

    Add comments and remove unused openStream function

--- database/dbseeding/writeAllToCSV.js
+++ database/dbseeding/writeAllToCSV.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const cliProgress = require('cli-progress');
 const { restaurant, dish, review, user } = require('./genDishesCSV');
 
+// Allows user to pass arguments to command line for different record types
 const model = process.argv[2] === 'restaurants' ? restaurant
   : process.argv[2] === 'dishes' ? dish
     : process.argv[2] === 'reviews' ? review
@@ -12,22 +13,17 @@ const model = process.argv[2] === 'restaurants' ? restaurant
 const numberOfRecords = parseInt(process.argv[3], 10);
 const isContinuation = process.argv[5] || false;
 
-// create new progress bar
+// create new progress bar for command line visualization
 const ProgressBar = new cliProgress.SingleBar({
   format: `|| {bar} || {percentage}% || {value}/{total} Records || ${model.table}`,
   stopOnComplete: true,
 }, cliProgress.Presets.shades_grey);
 ProgressBar.start(numberOfRecords, 0);
 
-// Function that returns a write stream to a file called prefixCOUNT.csv
-const filePath = `../${model.table}.csv`;
-const openStream = (filePath) => (
-  fs.createWriteStream(filePath, {
-    flags: 'a',
-  })
-);
+// filePath to be passed to createWriteStream
+const filePath = `../${model.table}7.csv`;
 
-// This function takes an array and turns it into a string without brackets
+// This function takes an array and turns it into a string without brackets or trailing comma
 const trimArray = (array) => {
   const stringified = JSON.stringify(array);
   return stringified.slice(1, stringified.length - 1);
@@ -36,6 +32,7 @@ const trimArray = (array) => {
 const writeRecords = (model) => {
   const writer = fs.createWriteStream(filePath, { flags: 'a' });
   writer.write(`${!isContinuation ? `${trimArray(model.headers)}\n` : ''}`, () => {
+    // have to open a second one because the first one kept dying early
     const writer2 = fs.createWriteStream(filePath, { flags: 'a' });
     for (let i = 0; i < numberOfRecords; i += 1) {
       writer2.write(`${trimArray(model())}${i === model.headers.length - 1 ? ',' : ''}\n`, () => {
