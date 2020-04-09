06c03b728b6101b2e71a44543bb314f503578c19
rory-roccio <rory.roccio@gmail.com>
2020-04-08 19:01:44 -0700

    Change dish price constraints, remove photos table

--- database/postgresql.sql
+++ database/postgresql.sql
@@ -3,40 +3,33 @@ CREATE TABLE "restaurants" (
   "restaurant_name" VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE "popular_dishes" (
+CREATE TABLE "dishes" (
   "dish_id" SERIAL PRIMARY KEY NOT NULL,
   "dish_name" VARCHAR(50) NOT NULL,
-  "price" DECIMAL(3,2) NOT NULL,
+  "price" DECIMAL(5,2) NOT NULL,
   "description" VARCHAR(1000) NOT NULL,
   "restaurant_id" INT
-);
-
-CREATE TABLE "photos" (
-  "photo_id" SERIAL PRIMARY KEY NOT NULL,
-  "url" VARCHAR(200) NOT NULL,
-  "caption" VARCHAR(1000) NOT NULL,
-  "review_id" INT
 );
 
 CREATE TABLE "users" (
   "user_id" SERIAL PRIMARY KEY NOT NULL,
   "userphoto" VARCHAR(200),
-  "username" VARCHAR(70) UNIQUE NOT NULL
+  "username" VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE "reviews" (
   "review_id" SERIAL PRIMARY KEY NOT NULL,
   "user_id" INT NOT NULL,
-  "date_time" DATETIME NOT NULL,
+  "url" VARCHAR(200) NOT NULL,
+  "caption" VARCHAR(1000) NOT NULL,
+  "date_time" TIMESTAMP NOT NULL,
   "rating" INT NOT NULL,
   "review_text" VARCHAR(1000) NOT NULL,
   "dish_id" INT NOT NULL
 );
 
-ALTER TABLE "popular_dishes" ADD FOREIGN KEY ("restaurant_id") REFERENCES "restaurants" ("restaurant_id");
-
-ALTER TABLE "photos" ADD FOREIGN KEY ("review_id") REFERENCES "reviews" ("review_id");
+ALTER TABLE "dishes" ADD FOREIGN KEY ("restaurant_id") REFERENCES "restaurants" ("restaurant_id");
 
 ALTER TABLE "reviews" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
 
-ALTER TABLE "reviews" ADD FOREIGN KEY ("dish_id") REFERENCES "popular_dishes" ("dish_id");
+ALTER TABLE "reviews" ADD FOREIGN KEY ("dish_id") REFERENCES "dishes" ("dish_id");
