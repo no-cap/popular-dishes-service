a4f10152346f4a1665e6fed088b1dfa35e2fb85e
rory-roccio <rory.roccio@gmail.com>
2020-04-12 19:01:36 -0700

    Change mongo schema to reflect new setup

--- database/dbseeding/mongoModels.js
+++ database/dbseeding/mongoModels.js
@@ -1,11 +1,33 @@
 const { Schema } = require('mongoose');
 const db = require('../mongoConnect.js');
 
+// const restaurantsSchema = new Schema({
+//   restaurantName: { type: String, required: true },
+//   dishes: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'dishes',
+//   }],
+// });
+
 const restaurantsSchema = new Schema({
   restaurantName: { type: String, required: true },
   dishes: [{
-    type: Schema.Types.ObjectId,
-    ref: 'dishes',
+    dishName: { type: String, required: true },
+    price: Number,
+    description: String,
+    reviews: [{
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+        required: true,
+      },
+      photoUrl: { type: String, required: true },
+      caption: String,
+      createdAt: Number,
+      updatedAt: Number,
+      rating: { type: Number, required: true },
+      reviewText: String,
+    }],
   }],
 });
 
