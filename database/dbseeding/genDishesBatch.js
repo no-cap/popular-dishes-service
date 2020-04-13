e83d0d94614a1f235e540c8b4e2f5096a250a5c3
rory-roccio <rory.roccio@gmail.com>
2020-04-13 13:34:21 -0700

    Remove Dishes and Reviews schema

--- database/dbseeding/genDishesBatch.js
+++ database/dbseeding/genDishesBatch.js
@@ -17,58 +17,27 @@ const prefixSet = prefixes[randomNum(0, 2)]; // gives each record a random prefi
 
 // Create containers for each record, these will later be add to bulk insertion ops
 const allRestaurants = [];
-const allDishes = [];
-const allReviews = [];
 const allUsers = [];
 
-// Generates a set of unique Ids
-const genIDs = (num) => {
-  const IDs = [];
-  for (let i = 0; i < num; i += 1) {
-    IDs.push(new ObjectID());
-  }
-  return IDs;
-};
-
-const someRestaurants = () => {
-  const restaurants = [];
-  for (let i = 0; i < NumberOfRestaurants; i += 1) {
-    const newRestaurant = {
-      _id: new ObjectID(),
-      restaurantName: faker.company.companyName(),
-      dishes: genIDs(randomNum(0, 8)),
-    };
-    restaurants.push(newRestaurant);
-    allRestaurants.push(newRestaurant);
-  }
-  return restaurants;
+const someUser = (review) => {
+  // console.log(review.userId);
+  const newUser = {
+    _id: review.userId,
+    username: faker.name.firstName(),
+    friendCount: randomNum(0, 200),
+    userPhoto: `https://sdc-users.s3-us-west-1.amazonaws.com/${randomNum(0, 71)}.png`,
+    reviews: [review.reviewId],
+  };
+  allUsers.push(newUser);
 };
 
 // Takes a restaurant and creates a set of popular dishes for that restaurant from its IDs
-const someDishes = (restaurant) => {
-  const dishes = [];
-  for (let i = 0; i < restaurant.dishes.length; i += 1) {
-    const newDish = {
-      _id: restaurant.dishes[i],
-      dishName: `${prefixSet[randomNum(0, prefixSet.length)]} ${foodSingular[randomNum(0, foodSingular.length)]}`,
-      price: `${randomNum(6, 25)}.${randomNum(80, 100)}`,
-      description: `${faker.lorem.words()} ${faker.lorem.words()}`,
-      restaurantId: restaurant._id,
-      reviews: genIDs(randomNum(0, 8)),
-    };
-    dishes.push(newDish);
-    allDishes.push(newDish);
-  }
-  return dishes;
-};
-
-const someReviews = (dish) => {
+const someReviews = (num) => {
   const reviews = [];
-  for (let i = 0; i < dish.reviews.length; i += 1) {
+  for (let i = 0; i < num; i += 1) {
     const newReview = {
-      _id: dish.reviews[i],
-      dishId: dish._id,
-      userId: genIDs(1)[0],
+      reviewId: new ObjectID(),
+      userId: new ObjectID(),
       photoUrl: `https://sdc-food.s3-us-west-1.amazonaws.com/${randomNum(0, 738)}.jpg`,
       caption: faker.lorem.words(),
       createdAt: Date.now(),
@@ -77,34 +46,41 @@ const someReviews = (dish) => {
       reviewText: faker.lorem.sentences(),
     };
     reviews.push(newReview);
-    allReviews.push(newReview);
+    someUser(newReview);
   }
   return reviews;
 };
 
-const someUser = (review) => {
-  const users = [];
-  const newUser = {
-    _id: review.userId,
-    username: faker.name.firstName(),
-    userPhoto: `https://sdc-users.s3-us-west-1.amazonaws.com/${randomNum(0, 71)}.png`,
-    reviews: [review._id],
-  };
-  users.push(newUser);
-  allUsers.push(newUser);
-  return users;
+const someDishes = (num) => {
+  const dishes = [];
+  for (let i = 0; i < num; i += 1) {
+    const noOfReviews = randomNum(0, 8);
+    const newDish = {
+      dishName: `${prefixSet[randomNum(0, prefixSet.length)]} ${foodSingular[randomNum(0, foodSingular.length)]}`,
+      price: `${randomNum(6, 25)}.${randomNum(80, 100)}`,
+      description: `${faker.lorem.words()} ${faker.lorem.words()}`,
+      reviews: someReviews(noOfReviews),
+    };
+    dishes.push(newDish);
+  }
+  return dishes;
 };
 
-const generateSet = () => {
-  someRestaurants().forEach((restaurant) => {
-    someDishes(restaurant).forEach((dish) => {
-      someReviews(dish).forEach((review) => {
-        someUser(review);
-      });
-    });
-  });
+const someRestaurants = () => {
+  for (let i = 0; i < NumberOfRestaurants; i += 1) {
+    const noOfDishes = randomNum(0, 8);
+    const newId = new ObjectID();
+    console.log(newId);
+    const newRestaurant = {
+      _id: newId,
+      restaurantName: faker.company.companyName(),
+      dishes: someDishes(noOfDishes),
+    };
+    allRestaurants.push(newRestaurant);
+  }
 };
-generateSet();
+
+someRestaurants();
 
 MongoClient.connect(process.env.MONGO_HOST, { useUnifiedTopology: true }, (err, client) => {
   if (err) console.error(`Failed to connect to database:\n${err}`);
@@ -113,37 +89,39 @@ MongoClient.connect(process.env.MONGO_HOST, { useUnifiedTopology: true }, (err, 
 
   // Initialize unordered Batches
   const restaurantsBatch = db.collection('restaurants').initializeUnorderedBulkOp({ useLegacyOps: true });
-  const dishesBatch = db.collection('dishes').initializeUnorderedBulkOp({ useLegacyOps: true });
-  const reviewsBatch = db.collection('reviews').initializeUnorderedBulkOp({ useLegacyOps: true });
   const usersBatch = db.collection('users').initializeUnorderedBulkOp({ useLegacyOps: true });
-  const allBatches = [restaurantsBatch, dishesBatch, reviewsBatch, usersBatch];
 
   // Add some operations to be appropriate batches...
   allRestaurants.forEach((restaurant) => {
     restaurantsBatch.insert(restaurant);
   });
-  allDishes.forEach((dish) => {
-    dishesBatch.insert(dish);
-  });
-  allReviews.forEach((review) => {
-    reviewsBatch.insert(review);
-  });
   allUsers.forEach((user) => {
     usersBatch.insert(user);
   });
-  // And execute batches...
+
   let successfulBatchInsertions = 0;
-  allBatches.forEach((batch) => {
-    batch.execute((err, result) => {
-      if (err) console.error(err);
-      // Check state of result
-      console.log(`Inserted ${result.nInserted} records`);
-      successfulBatchInsertions += 1;
-      // 4 successful insertions means all records have been inserted.
-      if (successfulBatchInsertions === 4) {
-        client.close();
-        // process.exit(1);
-      }
-    });
+  // And execute batches...
+  restaurantsBatch.execute((err, result) => {
+    if (err) console.error(err);
+    // Check state of result
+    console.log(`Inserted ${result.nInserted} restaurant records`);
+    successfulBatchInsertions += 1;
+    // 4 successful insertions means all records have been inserted.
+    if (successfulBatchInsertions === 2) {
+      client.close();
+      // process.exit(1);
+    }
+  });
+
+  usersBatch.execute((err, result) => {
+    if (err) console.error(err);
+    // Check state of result
+    console.log(`Inserted ${result.nInserted} user records`);
+    successfulBatchInsertions += 1;
+    // 4 successful insertions means all records have been inserted.
+    if (successfulBatchInsertions === 2) {
+      client.close();
+      // process.exit(1);
+    }
   });
 });
