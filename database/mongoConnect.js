87f631d6bd5e7fd0e35fe956e2dee106f1e84c92
rory-roccio <rory.roccio@gmail.com>
2020-04-17 23:07:27 +0000

    Add read preference to mongo URI

--- database/mongoConnect.js
+++ database/mongoConnect.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // Connection URL and database Name from .env
 const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST1, MONGO_HOST2, MONGO_HOST3, MONGO_DB } = process.env;
-const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST1},${MONGO_HOST2},${MONGO_HOST3}/${MONGO_DB}`;
+const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST1},${MONGO_HOST2},${MONGO_HOST3}/${MONGO_DB}?replicaSet=rs0&readPreference=secondaryPreferred`;
 
 const options = {
   useNewUrlParser: true,
@@ -14,7 +14,7 @@ const options = {
 // Create and export MongoDB connection
 module.exports = mongoose.createConnection(uri, options, (err) => {
   if (err) {
-    console.log(`Failed to connect to ${MONGO_DB}: \n${err}`);
+    console.log(`Failed to connect to ${MONGO_DB}: \n${err.stack}`);
   } else {
     console.log(`Successfully connected to ${MONGO_DB} as ${MONGO_USER}`);
   }
